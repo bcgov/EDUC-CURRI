b7e79cd8b8117788035e11ce5c3fba3a747b7302
Shaun Lum <shaun.lum@gov.bc.ca>
2019-10-01 09:42:31 -0700

    add

--- themes/curriculum/templates/views-view-unformatted--courses-by-grade-and-subject.tpl.php
+++ themes/curriculum/templates/views-view-unformatted--courses-by-grade-and-subject.tpl.php
@@ -35,8 +35,17 @@
 	   			$url_title =  preg_replace("/[\s_]/", "-", strtolower($title));
 				//remove special case urls
 				$url_str_remove_list = array("-(asl)");
+				
+				
 				$url_title = str_replace($url_str_remove_list , "", $url_title);
-				print '<div class="language-continuous-view"><div class="curriculum-course-title">Continuous Views '. $title .' 5-12:</div><div class="curriculum-download-links"><a href="/sites/curriculum.gov.bc.ca/files/curriculum/continuous-views/en_second-languages_5-12_'. $url_title . '_big-ideas.pdf" target="_blank">Big Ideas</a>' . ' | <a href="/sites/curriculum.gov.bc.ca/files/curriculum/continuous-views/en_second-languages_5-12_'. $url_title . '_curricular-competencies.pdf" target="_blank">Curricular Competencies</a> | <a href="/sites/curriculum.gov.bc.ca/files/curriculum/continuous-views/en_second-languages_5-12_'. $url_title . '_content.pdf" target="_blank">Content</a> </div></div>';
+				$url_title = str_replace("é" , "e", $url_title);
+				$url_title = str_replace("ç" , "c", $url_title);
+				global $language;
+			   
+				print '<div class="language-continuous-view"><div class="curriculum-course-title">' . ($language->language == "en" ? 'Continuous Views' : 'Grilles des cheminements') . ' '. $title .' 5-12:</div><div class="curriculum-download-links"><a href="/sites/curriculum.gov.bc.ca/files/curriculum/continuous-views/'.$language->language .'_second-languages_5-12_'. $url_title . '_big-ideas.pdf" target="_blank">' . ($language->language == "en" ? 'Big ideas' : 'Grandes idées') .'</a>' . ' | <a href="/sites/curriculum.gov.bc.ca/files/curriculum/continuous-views/'. $language->language .'_second-languages_5-12_'. $url_title . '_curricular-competencies.pdf" target="_blank">' . ($language->language == "en" ? 'Curricular Competencies' : 'Compétences disciplinaires') . '</a> | <a href="/sites/curriculum.gov.bc.ca/files/curriculum/continuous-views/'.$language->language.'_second-languages_5-12_'. $url_title . '_content.pdf" target="_blank">' . ($language->language == "en" ? 'Content' : 'Contenu') . '</a> </div></div>';
+				
+				
+				
 			}
 	   ?>
 
